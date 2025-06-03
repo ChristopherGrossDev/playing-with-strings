7bf121665ad3fd157aaf89837880e7b11b6fac87
Christopher.gross.dev <christopher.gross.dev@gmail.com>
2025-06-03 08:05:37 -0500

    29 passing

--- main.js
+++ main.js
@@ -1,6 +1,182 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function xify(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    result += 'x';
+  }
+  return result;
+}
+
+function yellingChars(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    result += str[i] + '!';
+  }
+  return result;
+}
+
+function indexedChars(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    result += i + str[i];
+  }
+  return result;
+}
+
+function numberedChars(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    result += `(${i + 1})` + str[i];
+  }
+  return result;
+}
+
+function exclaim(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '?' || str[i] === '.') {
+      result += '!';
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
+
+function repeatIt(str, n) {
+  let result = '';
+  for (let i = 0; i < n; i++) {
+    result += str;
+  }
+  return result;
+}
+
+function truncate(str) {
+  if (str.length > 18) {
+    let result = '';
+    for (let i = 0; i < 15; i++) {
+      result += str[i];
+    }
+    return result + '...';
+  } else {
+    return str;
+  }
+}
+
+function emailify(str) {
+  let first = '';
+  let last = '';
+  let spaceFound = false;
+  for (let i = 0; i < str.length; i++) {
+    let c = str[i].toLowerCase();
+    if (c === ' ') {
+      spaceFound = true;
+    } else if (!spaceFound) {
+      first += c;
+    } else {
+      last += c;
+    }
+  }
+  return first[0] + last + '.prsvr@gmail.com';
+}
+
+function reverse(str) {
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  return result;
+}
+
+function onlyVowels(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    let c = str[i].toLowerCase();
+    if (c === 'a' || c === 'e' || c === 'i' || c === 'o' || c === 'u') {
+      result += str[i];
+    }
+  }
+  return result;
+}
+
+// Stretch Goals
+
+function crazyCase(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    result += i % 2 === 0
+      ? str[i].toLowerCase()
+      : str[i].toUpperCase();
+  }
+  return result;
+}
+
+function titleCase(str) {
+  let result = '';
+  let newWord = true;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ' ') {
+      result += str[i];
+      newWord = true;
+    } else {
+      result += newWord
+        ? str[i].toUpperCase()
+        : str[i].toLowerCase();
+      newWord = false;
+    }
+  }
+  return result;
+}
+
+function camelCase(str) {
+  let result = '';
+  let newWord = false;
+  for (let i = 0; i < str.length; i++) {
+    let c = str[i];
+    if (c === ' ') {
+      newWord = true;
+    } else {
+      result += newWord ? c.toUpperCase() : c.toLowerCase();
+      newWord = false;
+    }
+  }
+  return result;
+}
+
+function crazyCase2ReturnOfCrazyCase(str) {
+  let result = '';
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    let c = str[i];
+    if (c === ' ') {
+      result += c;
+    } else {
+      result += count % 2 === 0 ? c.toLowerCase() : c.toUpperCase();
+      count++;
+    }
+  }
+  return result;
+}
+
+module.exports = {
+  xify,
+  yellingChars,
+  indexedChars,
+  numberedChars,
+  exclaim,
+  repeatIt,
+  truncate,
+  emailify,
+  reverse,
+  onlyVowels,
+  crazyCase,
+  titleCase,
+  camelCase,
+  crazyCase2ReturnOfCrazyCase,
+};
+
 
 
 
